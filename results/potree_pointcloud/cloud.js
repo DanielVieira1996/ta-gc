56f0edb6df187ea41bee9b6425f01829e2448470
DanielVieira1996 <danyvieira_96@hotmail.com>
2019-05-22 21:51:57 +0100

    Results feito
    Results feito

---
+++ results/potree_pointcloud/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "points": 12674448,
+    "boundingBox": {
+        "lx": 554034.85,
+        "ly": 4505755.24,
+        "lz": -18.47,
+        "ux": 554444.89,
+        "uy": 4506165.28,
+        "uz": 391.5700000000372
+    },
+    "tightBoundingBox": {
+        "lx": 554034.85,
+        "ly": 4505755.24,
+        "lz": -18.47,
+        "ux": 554444.89,
+        "uy": 4506088.87,
+        "uz": 63.42
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED",
+        "CLASSIFICATION"
+    ],
+    "spacing": 2.8408403396606447,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
